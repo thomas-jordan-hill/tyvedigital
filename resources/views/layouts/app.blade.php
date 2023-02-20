f1656af35cb269cebfc4fd20ec0448e6a6f3ed16
Thomas Hill <thomashill@Thomass-MacBook-Pro.local>
2023-02-20 02:33:11 +0000

    1.0.52

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('../favicon/apple-touch-icon.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('../favicon/favicon-32x32.png') }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('../favicon/favicon-16x16.png') }}">
-        <link rel="manifest" href="{{ asset('../favicon/site.webmanifest') }}">
-        <link rel="mask-icon" href="{{ asset('../favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
-        <link rel="shortcut icon" href="{{ asset('../favicon/favicon.ico') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/storage/favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/storage/favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/storage/favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('/storage/favicon/site.webmanifest') }}">
+        <link rel="mask-icon" href="{{ asset('/storage/favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
+        <link rel="shortcut icon" href="{{ asset('/storage/favicon/favicon.ico') }}">
         <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="msapplication-config" content="{{ asset('../favicon/browserconfig.xml') }}">
+        <meta name="msapplication-config" content="{{ asset('/storage/favicon/browserconfig.xml') }}">
         <meta name="theme-color" content="#ffffff">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
