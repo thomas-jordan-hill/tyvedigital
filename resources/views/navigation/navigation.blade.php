d971c645a8b94d9ae9b09daccbf00c938bd21a1d
Thomas Hill <thomashill@Thomass-MacBook-Pro.local>
2023-02-26 12:49:33 +0000

    1.0.60

--- resources/views/navigation/navigation.blade.php
+++ resources/views/navigation/navigation.blade.php
@@ -1,5 +1,5 @@
 <nav class="w-100 navbar navbar-expand-lg navbar-light mt-0 mt-md-4 p-0">
-  <div class="d-block d-md-none w-100 p-0 mt-2 mb-1 text-end"><p id="navbar-contact" class="p card-title"><small><a class="colour3 text-decoration-none" href="tel:+447585221672">Get digital: 07585221672</a></small></p></div>
+  <div class="d-block d-md-none w-100 p-0 mt-2 mb-1 text-end"><p id="navbar-contact" class="p card-title"><small><a class="colour3 text-decoration-none" href="tel:+447585221672">Contact us today: 07585221672</a></small></p></div>
   <a style="min-width: 175px;" class="navbar-brand ms-0 me-4" href="/"><img class="logo-main w-100" src="/storage/logo/tyvedigital-logo.svg"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <!--<span class="navbar-toggler-icon"></span>-->Menu
