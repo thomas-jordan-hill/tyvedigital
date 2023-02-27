946edf954fcadda2aafa9d75a08d21c9732f7056
Thomas Hill <thomashill@Thomass-MacBook-Pro.local>
2023-02-27 11:23:46 +0000

    1.0.67

--- resources/views/services/websites.blade.php
+++ resources/views/services/websites.blade.php
@@ -36,9 +36,11 @@
                                 <h4 class="h4 colour3 mb-4"><small>We've worked for a range of clients from sole traders, to larger sized city businesses.</small></h4>
                                 <img class="w-100 img-fluid d-none d-md-block mt-0 mt-md-5 pb-0 pb-md-0" style="width: 100%!important;" src="/storage/images/transparent-website-example2-tyvedigital.png">
                                 <img class="w-100 img-fluid d-block d-md-none rounded-lg" src="/storage/images/transparent-website-example2-tyvedigital.png">
-                                <h4 class="d-none d-md-block card-title fw-bold colour3 mb-0">We'd love to help you digitalise customer journey.</h4>
-                                <h5 class="d-none d-md-block card-title colour3 mt-2 mb-4">Get in touch today.</h5>
-                                <a href="/contact/" class="d-none d-md-block ml-auto my-auto align-middle btn btn-primary">Contact Us</a>
+                                <div class="d-none d-md-block ">
+                                    <h4 class="card-title fw-bold colour3 mb-0">We'd love to help you digitalise customer journey.</h4>
+                                    <h5 class="card-title colour3 mt-2 mb-4">Get in touch today.</h5>
+                                    <a href="/contact/" class="ml-auto my-auto align-middle btn btn-primary">Contact Us</a>
+                                </div>
                             </div>
                             <div class="col mb-4 mt-0">
                                 <img class="w-100 img-fluid d-none d-md-block pb-5" style="width: 100%!important;" src="/storage/images/transparent-website-example-tyvedigital.png">
